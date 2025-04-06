440f272d2093716a9fc4e153194fa0fb0d0dafb1
Ihor Akhypenko <wasbornindecember@gmail.com>
2025-04-06 19:48:09 +0300

    Delete Product feature added. Postman collection updated.

--- src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -11,11 +11,13 @@ public record UpdateProductCommand(
 
 public record UpdateProductResult(bool IsSuccess);
 
-public class UpdateProductCommandHandler(IDocumentSession session, ILogger<UpdateProductCommandHandler> logger)
+internal class UpdateProductCommandHandler(IDocumentSession session, ILogger<UpdateProductCommandHandler> logger)
     : ICommandHandler<UpdateProductCommand, UpdateProductResult>
 {
     public async Task<UpdateProductResult> Handle(UpdateProductCommand command, CancellationToken cancellationToken)
     {
+        logger.LogInformation("UpdateProductCommandHandler. Handle called with {@Command}", command);
+
         var product = await session.LoadAsync<Product>(command.Id, cancellationToken)
                       ?? throw new ProductNotFoundException();
 
