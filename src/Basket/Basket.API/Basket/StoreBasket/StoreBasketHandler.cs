d8b7011c71ad76056b019a2a526bd02dbc274ae4
Ihor Arkhypenko <iarkhypenko@teamworkcommerce.com>
2025-04-29 16:34:28 +0300

    StoreBasket ready.

---
+++ src/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -0,0 +1,27 @@
+﻿namespace Basket.API.Basket.StoreBasket;
+
+public record StoreBasketCommand(ShoppingCart ShoppingCart) : ICommand<StoreBasketResult>;
+
+public record StoreBasketResult(string UserName);
+
+public class StoreBasketCommandValidator : AbstractValidator<StoreBasketCommand>
+{
+    public StoreBasketCommandValidator()
+    {
+        RuleFor(x => x.ShoppingCart).NotNull().WithMessage("Shopping cart can not be null.");
+        RuleFor(x => x.ShoppingCart.UserName).NotEmpty().WithMessage("UserName is required.");
+    }
+}
+
+public class StoreBasketCommandHandler : ICommandHandler<StoreBasketCommand, StoreBasketResult>
+{
+    public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
+    {
+        var shoppingCart = command.ShoppingCart;
+
+        // TODO: Store Basket in database.
+        // TODO: Update cache.
+
+        return new StoreBasketResult("temp");
+    }
+}
