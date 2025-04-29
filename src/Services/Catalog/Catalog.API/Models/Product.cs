6303d8289c4f516acdd2a98cb579b8aa325a0d20
Ihor Arkhypenko <iarkhypenko@teamworkcommerce.com>
2025-04-29 14:37:29 +0300

    launchSettings updated. Domain models for basket created.

--- src/Services/Catalog/Catalog.API/Models/Product.cs
+++ src/Services/Catalog/Catalog.API/Models/Product.cs
@@ -3,14 +3,9 @@
 public class Product
 {
     public Guid Id { get; set; }
-
     public string Name { get; set; } = null!;
-
     public List<string> Category { get; set; } = [];
-
     public string Description { get; set; } = null!;
-
     public string ImageFile { get; set; } = null!;
-
     public decimal Price { get; set; }
 }
