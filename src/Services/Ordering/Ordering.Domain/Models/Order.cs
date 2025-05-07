18a10edc449171f9722747e6cd41d3436d476a56
Ihor Arkhypenko <iarkhypenko@teamworkcommerce.com>
2025-05-07 15:10:15 +0300

    Domain models created.

---
+++ src/Services/Ordering/Ordering.Domain/Models/Order.cs
@@ -0,0 +1,21 @@
+﻿namespace Ordering.Domain.Models;
+
+public class Order : Aggregate<Guid>
+{
+    private readonly List<OrderItem> _orderItems = [];
+    public IReadOnlyList<OrderItem> OrderItems => _orderItems.AsReadOnly();
+
+    public Guid CustomerId { get; private set; }
+    public string OrderName { get; private set; } = null!;
+    public Address ShippingAdress { get; private set; } = null!;
+    public Address BillingAdress { get; private set; } = null!;
+    public Payment Payment { get; private set; } = null!;
+    public OrderStatus Status { get; private set; } = OrderStatus.Pending;
+
+    public decimal TotalPrice
+    {
+        get => OrderItems.Sum(x => x.Price * x.Quantity);
+        private set { }
+    }
+
+}
---
+++ src/Services/Ordering/Ordering.Domain/Models/ValueObjects/Payment.cs
@@ -0,0 +1,10 @@
+﻿namespace Ordering.Domain.Models.ValueObjects;
+
+public class Payment
+{
+    public string? CardName { get; } = null!;
+    public string CardNumber { get; } = null!;
+    public string Expiration { get; } = null!;
+    public string CVV { get; } = null!;
+    public int PaymentMethod { get; }
+}
