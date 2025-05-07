de38972f72ba9eb547189987df87f95f17b24b7d
Ihor Arkhypenko <iarkhypenko@teamworkcommerce.com>
2025-05-07 16:16:52 +0300

    Domain layer ready.

--- src/Services/Ordering/Ordering.Domain/Models/Order.cs
+++ src/Services/Ordering/Ordering.Domain/Models/Order.cs
@@ -1,14 +1,14 @@
 ﻿namespace Ordering.Domain.Models;
 
-public class Order : Aggregate<Guid>
+public class Order : Aggregate<OrderId>
 {
     private readonly List<OrderItem> _orderItems = [];
     public IReadOnlyList<OrderItem> OrderItems => _orderItems.AsReadOnly();
 
-    public Guid CustomerId { get; private set; }
-    public string OrderName { get; private set; } = null!;
-    public Address ShippingAdress { get; private set; } = null!;
-    public Address BillingAdress { get; private set; } = null!;
+    public CustomerId CustomerId { get; private set; } = null!;
+    public OrderName OrderName { get; private set; } = null!;
+    public Address ShippingAddress { get; private set; } = null!;
+    public Address BillingAddress { get; private set; } = null!;
     public Payment Payment { get; private set; } = null!;
     public OrderStatus Status { get; private set; } = OrderStatus.Pending;
 
@@ -18,4 +18,57 @@ public class Order : Aggregate<Guid>
         private set { }
     }
 
+    public static Order Create(
+        OrderId id,
+        CustomerId customerId,
+        OrderName orderName,
+        Address shippingAddress,
+        Address billingAddress,
+        Payment payment)
+    {
+        var order = new Order
+        {
+            Id = id,
+            CustomerId = customerId,
+            OrderName = orderName,
+            ShippingAddress = shippingAddress,
+            BillingAddress = billingAddress,
+            Payment = payment,
+            Status = OrderStatus.Pending
+        };
+
+        order.AddDomainEvent(new OrderCreatedEvent(order));
+
+        return order;
+    }
+
+    public void Update(OrderName orderName, Address shippingAddress, Address billingAddress, Payment payment,
+        OrderStatus status)
+    {
+        OrderName = orderName;
+        ShippingAddress = shippingAddress;
+        BillingAddress = billingAddress;
+        Payment = payment;
+        Status = status;
+
+        AddDomainEvent(new OrderUpdatedEvent(this));
+    }
+
+    public void Add(ProductId productId, int quantity, decimal price)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(quantity);
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
+
+        var orderItem = new OrderItem(Id, productId, quantity, price);
+        _orderItems.Add(orderItem);
+    }
+
+    public void Remove(ProductId productId)
+    {
+        var orderItem = _orderItems.FirstOrDefault(x => x.ProductId == productId);
+        if (orderItem is not null)
+        {
+            _orderItems.Remove(orderItem);
+        }
+    }
 }
--- src/Services/Ordering/Ordering.Domain/Models/ValueObjects/Payment.cs
+++ src/Services/Ordering/Ordering.Domain/Models/ValueObjects/Payment.cs
@@ -6,5 +6,28 @@ public class Payment
     public string CardNumber { get; } = null!;
     public string Expiration { get; } = null!;
     public string CVV { get; } = null!;
-    public int PaymentMethod { get; }
+    public int PaymentMethod { get; } = default;
+
+    protected Payment()
+    {
+    }
+
+    private Payment(string cardName, string cardNumber, string expiration, string cvv, int paymentMethod)
+    {
+        CardName = cardName;
+        CardNumber = cardNumber;
+        Expiration = expiration;
+        CVV = cvv;
+        PaymentMethod = paymentMethod;
+    }
+
+    public static Payment Of(string cardName, string cardNumber, string expiration, string cvv, int paymentMethod)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(cardName);
+        ArgumentException.ThrowIfNullOrWhiteSpace(cardNumber);
+        ArgumentException.ThrowIfNullOrWhiteSpace(cvv);
+        ArgumentOutOfRangeException.ThrowIfGreaterThan(cvv.Length, 3);
+
+        return new Payment(cardName, cardNumber, expiration, cvv, paymentMethod);
+    }
 }
