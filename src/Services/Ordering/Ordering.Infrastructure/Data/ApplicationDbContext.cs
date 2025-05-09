edd2d2ec53dba5266b8034b03172aa49f5ec2b38
Ihor Arkhypenko <iarkhypenko@teamworkcommerce.com>
2025-05-09 15:55:48 +0300

    Ordering.Infrastructure DbContext created, configurations for entities created.

---
+++ src/Services/Ordering/Ordering.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using Ordering.Domain.Models;
+
+namespace Ordering.Infrastructure.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    // TODO: Try to replace it by simple properties with get and set.
+    public DbSet<Customer> Customers => Set<Customer>();
+    public DbSet<Product> Products => Set<Product>();
+    public DbSet<Order> Orders => Set<Order>();
+    public DbSet<OrderItem> OrderItems => Set<OrderItem>();
+
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        base.OnModelCreating(modelBuilder);
+    }
+}
