2584197f2700c47289e3984467d02f09770e6659
Ihor Arkhypenko <iarkhypenko@teamworkcommerce.com>
2025-05-13 12:28:32 +0300

    EventHandlers for created/updated order events ready.

---
+++ src/Services/Ordering/Ordering.Application/Orders/EventHandlers/OrderCreatedEventHandler.cs
@@ -0,0 +1,11 @@
+﻿namespace Ordering.Application.Orders.EventHandlers;
+
+public class OrderCreatedEventHandler(ILogger<OrderCreatedEventHandler> logger)
+    : INotificationHandler<OrderCreatedEvent>
+{
+    public Task Handle(OrderCreatedEvent orderCreatedEvent, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("Domain Event handled: {DomainEvent}", orderCreatedEvent.GetType().Name);
+        return Task.CompletedTask;
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Orders/EventHandlers/OrderUpdatedEventHandler.cs
@@ -0,0 +1,11 @@
+﻿namespace Ordering.Application.Orders.EventHandlers;
+
+public class OrderUpdatedEventHandler(ILogger<OrderUpdatedEventHandler> logger)
+    : INotificationHandler<OrderUpdatedEvent>
+{
+    public Task Handle(OrderUpdatedEvent orderUpdatedEvent, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("Domain Event handled: {DomainEvent}", orderUpdatedEvent.GetType().Name);
+        return Task.CompletedTask;
+    }
+}
